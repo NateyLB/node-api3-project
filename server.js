935b683ef357a94e8d5335f776c39525b5971db4
Nathan <nateyl.b@gmail.com>
2020-05-06 16:14:14 -0700

    finishd w/ stretch

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
+const postRoutes = require('./posts/postRouter.js');
+const userRoutes = require('./users/userRouter.js');
+
 const server = express();
+
+//global middleware 
+server.use(express.json()); // built in middleware, no need to npm install it
+server.use(logger);
+server.use('/posts', postRoutes);
+server.use('/users', userRoutes);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -10,9 +20,7 @@ server.get('/', (req, res) => {
 
 function logger(req, res, next) {
   console.log(
-    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
-      'Origin'
-    )}`
+    `[${new Date().toISOString()}] ${req.method} to ${req.url}`
   );
 
   next();
